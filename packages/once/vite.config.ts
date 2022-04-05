365f0bffbbe4c22d31e9f479e73f36c83ee9ba2e
Woldtwerk <woldt.willi@gmail.com>
2022-04-05 09:00:29 +0200

    fix: 🐛 fix exports field in once package

--- packages/once/vite.config.ts
+++ packages/once/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       // name: 'DrupalOnce',
-      fileName: 'index.js',
+      fileName: 'index',
       formats: ['es']
     },
   },
